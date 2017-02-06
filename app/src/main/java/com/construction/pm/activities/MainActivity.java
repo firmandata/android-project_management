eb50c4bb1e2fae95444913266d7e5a17628d81c6
Firmansyah <firmansyah@indosystem.com>
2017-02-06 08:10:03 +0700

    Main Layout, Change Profile, Change Password

--- app/src/main/java/com/construction/pm/activities/MainActivity.java
+++ app/src/main/java/com/construction/pm/activities/MainActivity.java
@@ -1,14 +1,17 @@
 package com.construction.pm.activities;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.construction.pm.R;
 import com.construction.pm.views.MainLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     protected MainLayout mMainLayout;
 
@@ -16,8 +19,45 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // -- Prepare MainLayout --
         mMainLayout = MainLayout.buildMainLayout(this, null);
+        mMainLayout.getNavigationView().setNavigationItemSelectedListener(this);
+
+        // -- Load MainLayout to activity --
         mMainLayout.loadLayoutToActivity(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.navigator_menu_inbox:
+                break;
+            case R.id.navigator_menu_project_task:
+                break;
+            case R.id.navigator_menu_monitoring:
+                break;
+            case R.id.navigator_menu_update_task_progress:
+                break;
+            case R.id.navigator_menu_request_report:
+                break;
+            case R.id.navigator_menu_profile:
+                mMainLayout.showUserChangeProfile();
+                break;
+            case R.id.navigator_menu_change_password:
+                mMainLayout.showUserChangePassword();
+                break;
+            case R.id.navigator_menu_logout:
+                break;
+        }
+
+        if (menuItem.isCheckable()) {
+            if (!menuItem.isChecked())
+                menuItem.setChecked(true);
+        }
+
+        mMainLayout.getLayout().closeDrawers();
+
+        return true;
     }
 
     @Override
--- app/src/main/java/com/construction/pm/views/MainLayout.java
+++ app/src/main/java/com/construction/pm/views/MainLayout.java
@@ -1,7 +1,10 @@
 package com.construction.pm.views;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,12 +17,20 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.construction.pm.R;
+import com.construction.pm.fragments.UserChangePasswordFragment;
+import com.construction.pm.fragments.UserChangeProfileFragment;
 
 public class MainLayout {
 
     protected Context mContext;
 
+    protected AppCompatActivity mActivity;
+    protected Handler mActivityHandler;
+    protected static final String FRAGMENT_USER_CHANGE_PROFILE = "FRAGMENT_USER_CHANGE_PROFILE";
+    protected static final String FRAGMENT_USER_CHANGE_PASSWORD = "FRAGMENT_USER_CHANGE_PASSWORD";
+
     protected DrawerLayout mMainLayout;
+    protected NavigationView mNavigationView;
     protected Toolbar mToolbar;
     protected ActionBarDrawerToggle mActionBarDrawerToggle;
 
@@ -44,6 +55,7 @@ public class MainLayout {
     protected void initializeView(final DrawerLayout mainLayout) {
         mMainLayout = mainLayout;
         mToolbar = (Toolbar) mMainLayout.findViewById(R.id.contentToolbar);
+        mNavigationView = (NavigationView) mMainLayout.findViewById(R.id.navigator);
     }
 
     public DrawerLayout getLayout() {
@@ -54,16 +66,22 @@ public class MainLayout {
         return mToolbar;
     }
 
-    public void loadLayoutToActivity(AppCompatActivity activity) {
-        activity.setContentView(mMainLayout);
+    public NavigationView getNavigationView() {
+        return mNavigationView;
+    }
 
-        activity.setSupportActionBar(mToolbar);
-        ActionBar actionBar = activity.getSupportActionBar();
+    public void loadLayoutToActivity(AppCompatActivity activity) {
+        mActivity = activity;
+
+        mActivity.setContentView(mMainLayout);
+
+        mActivity.setSupportActionBar(mToolbar);
+        ActionBar actionBar = mActivity.getSupportActionBar();
         if (actionBar != null) {
             actionBar.setTitle(R.string.app_name);
         }
 
-        mActionBarDrawerToggle = new ActionBarDrawerToggle(activity, mMainLayout, mToolbar, R.string.main_drawer_open, R.string.main_drawer_close) {
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(mActivity, mMainLayout, mToolbar, R.string.main_drawer_open, R.string.main_drawer_close) {
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
@@ -77,5 +95,41 @@ public class MainLayout {
         mMainLayout.addDrawerListener(mActionBarDrawerToggle);
 
         mActionBarDrawerToggle.syncState();
+
+        mActivityHandler = new Handler();
+    }
+
+    protected void loadFragment(final Fragment fragment, final String tag) {
+        if (mActivityHandler == null)
+            return;
+
+        mActivityHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                if (mActivity == null)
+                    return;
+
+                FragmentTransaction fragmentTransaction = mActivity.getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.contentBodyHolder, fragment, tag);
+                fragmentTransaction.commitAllowingStateLoss();
+            }
+        });
+    }
+
+    public UserChangeProfileFragment showUserChangeProfile() {
+        UserChangeProfileFragment userChangeProfileFragment = UserChangeProfileFragment.newInstance();
+
+        loadFragment(userChangeProfileFragment, FRAGMENT_USER_CHANGE_PROFILE);
+
+        return userChangeProfileFragment;
+    }
+
+    public UserChangePasswordFragment showUserChangePassword() {
+        UserChangePasswordFragment userChangePasswordFragment = UserChangePasswordFragment.newInstance();
+
+        loadFragment(userChangePasswordFragment, FRAGMENT_USER_CHANGE_PASSWORD);
+
+        return userChangePasswordFragment;
     }
 }
