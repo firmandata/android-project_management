8f96f5bde230b5e66e7bc2964e81f96c75e3898b
Firmansyah <firmansyah@indosystem.com>
2017-04-02 19:23:26 +0700

    Revert "Set recyclable false to list image view"
    This reverts commit e25558f511f6522146c8fda8d90bc238b635f0a4.

--- app/src/main/java/com/construction/pm/views/project_activity/ProjectActivityMonitoringListView.java
+++ app/src/main/java/com/construction/pm/views/project_activity/ProjectActivityMonitoringListView.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.construction.pm.R;
@@ -233,7 +234,6 @@ public class ProjectActivityMonitoringListView {
                 return;
 
             ProjectActivityMonitoringModel projectActivityMonitoringModel = mProjectActivityMonitoringModelList.get(position);
-            holder.setIsRecyclable(false);
             holder.setImageRequestListener(mImageRequestListener);
             holder.setProjectActivityMonitoringModel(projectActivityMonitoringModel);
         }
