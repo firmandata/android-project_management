e25558f511f6522146c8fda8d90bc238b635f0a4
Firmansyah <firmansyah@indosystem.com>
2017-04-02 19:03:01 +0700

    Set recyclable false to list image view

--- app/src/main/java/com/construction/pm/views/project_activity/ProjectActivityMonitoringListView.java
+++ app/src/main/java/com/construction/pm/views/project_activity/ProjectActivityMonitoringListView.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.construction.pm.R;
@@ -234,6 +233,7 @@ public class ProjectActivityMonitoringListView {
                 return;
 
             ProjectActivityMonitoringModel projectActivityMonitoringModel = mProjectActivityMonitoringModelList.get(position);
+            holder.setIsRecyclable(false);
             holder.setImageRequestListener(mImageRequestListener);
             holder.setProjectActivityMonitoringModel(projectActivityMonitoringModel);
         }
